31381d9defd9f40898140ea31ff5fecd81c2467d
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-11-04 22:41:49 +0530

    added controllers skeletons

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,9 @@
+class LocationsController < ApplicationController
+
+  def index
+    @locations = Location.all
+    respond_to do |format|
+      format.json {render json:@locations}
+    end
+  end
+end
