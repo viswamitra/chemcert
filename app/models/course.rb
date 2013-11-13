ae6c81f62b36c3fbcfcc22b00e1c635d38da1095
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-11-13 23:20:07 +0530

    added trainers

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ActiveRecord::Base
   belongs_to :training_organization
   belongs_to :location_relation
   belongs_to :venue
+  belongs_to :trainer
 
   accepts_nested_attributes_for :location_relation
   accepts_nested_attributes_for :training_organization
