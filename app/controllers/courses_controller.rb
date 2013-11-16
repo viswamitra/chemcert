30fd33a11ebfbddcc8d3b7620094ec7953bca204
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-11-16 21:37:50 +0530

    added feature to edit and add new course to courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -58,24 +58,35 @@ class CoursesController < ApplicationController
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
-    @rto = TrainingOrganization.find(params[:id])
+    @course = Course.find(params[:id])
+    state_id = params[:course][:state]
+    town_id = params[:course][:town]
+    postal_code_id = params[:course][:postal_code]
+    location_relation_id = LocationRelation.where(:state_id => state_id, :postal_code_id => postal_code_id, :town_id => town_id).first
     respond_to do |format|
-      if @rto.update(rto_params)
-        format.html { redirect_to @rto, notice: 'RTO was successfully updated.' }
+      if @course.update(:training_organization_id => params[:course][:training_organization],
+                     :is_correspondence => params[:course][:is_correspondence],
+                     :course_code => params[:course][:course_code],
+                     :course_date => params[:course][:course_date],
+                     :location_relation_id => location_relation_id.id,
+                     :venue_id => params[:course][:venue],
+                     :trainer_id => params[:course][:trainer]
+      )
+        format.html { redirect_to @course, notice: 'RTO was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @rto.errors, status: :unprocessable_entity }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /products/1
   def destroy
-    @rto = TrainingOrganization.find(params[:id])
-    @rto.destroy
+    @course = Course.find(params[:id])
+    @course.destroy
     respond_to do |format|
-      format.html { redirect_to training_organizations_url }
+      format.html { redirect_to courses_url }
       format.json { head :no_content }
     end
   end
@@ -85,6 +96,4 @@ class CoursesController < ApplicationController
   def set_rto
     @rto = TrainingOrganization.find(params[:id])
   end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
 end
