0124aa7ccbc088fed9f986c120555b6139f5b674
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-11-22 00:15:10 +0530

    added venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -19,11 +19,31 @@ class VenuesController < ApplicationController
   #get /venues/1/edit
   def edit
     @venue = Venue.find(params[:id])
+    @postal_code_selected = @venue.location_relation.postal_code
+    @state_selected = @venue.location_relation.state
+    @town_selected = @venue.location_relation.town
   end
 
   #post /venue
   def create
-    @venue = Venue.new(rto_params)
+    state_id = params[:venue][:state]
+    town_id = params[:venue][:town]
+    postal_code_id = params[:venue][:postal_code]
+    location_relation = LocationRelation.where(:state_id => state_id, :postal_code_id => postal_code_id, :town_id => town_id).first
+    @venue = Venue.new(
+      :name => params[:venue][:name],
+      :address => params[:venue][:address],
+      :location_relation_id => location_relation.id,
+      :room_name => params[:venue][:room_name],
+      :room_cost => params[:venue][:room_cost],
+      :room_setup => params[:venue][:room_setup],
+      :capacity => params[:venue][:capacity],
+      :screen => params[:venue][:screen],
+      :whiteboard_available => params[:venue][:whiteboard_available],
+      :catering => params[:venue][:catering],
+      :lunch_available => params[:venue][:lunch],
+      :notes => params[:venue][:notes]
+    )
 
     respond_to do |format|
       if @venue.save
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,10 @@
 class Venue < ActiveRecord::Base
+
+  attr_accessible :name, :address, :room_name, :room_cost, :room_setup, :capacity, :location_relation_id,
+                  :screen, :whiteboard_available, :catering, :lunch_available, :notes
+
   belongs_to :location_relation
   has_many :courses
+
+  validates_presence_of :name
 end
