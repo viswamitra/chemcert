4ef67c95c3da34598d4a4b27deb3a808f3362b23
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-12-19 19:21:10 +0530

    changed addition => additoinal

--- app/models/student_course_detail.rb
+++ app/models/student_course_detail.rb
@@ -1,7 +1,7 @@
 class StudentCourseDetail < ActiveRecord::Base
 
   attr_accessible :student_id,:course_id, :student_course_id,
-                  :enrolled_at, :student_course_id, :addition_module_id,
+                  :enrolled_at, :student_course_id, :additional_module_id,
                   :industry, :needs_specify, :comments,
                   :course_fee, :payment_method, :payment_date,
                   :paid, :current_accreditation, :current_expiry_date, :enquiry, :result, :special_modules
