85a337b3b093ea99500b383f8e87e2bd212efd9d
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-12-19 19:23:30 +0530

    removed a migration

--- db/migrate/20131215095601_change_additional_module_name_in_student_course_detail.rb
+++
@@ -1,5 +0,0 @@
-class ChangeAdditionalModuleNameInStudentCourseDetail < ActiveRecord::Migration
-  def change
-    rename_column :student_course_details, :addition_module_id, :additional_module_id
-  end
-end
