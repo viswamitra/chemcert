41972ff26f7441f799d8692b4a62b27f46a2708d
abhiram viswamitra <abhiram.viswamitra@gmail.com>
2013-12-20 18:52:56 +0530

    added changes to venues bug

--- app/models/location_relation.rb
+++ app/models/location_relation.rb
@@ -18,7 +18,9 @@ class LocationRelation < ActiveRecord::Base
        :town => lr.town.name,
        :state_id => lr.state.id,
        :state => lr.state.name,
-       :venue => lr.venue.try(:name)}
+       :venue_id => lr.venue.try(:id),
+       :venue => lr.venue.try(:name)
+       }
     end
   end
 
